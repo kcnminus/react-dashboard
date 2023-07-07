596124a806854493ea7c90bd554d162577407a3d
Colin No <colin.no@policygenius.com>
2023-07-07 17:04:18 -0400

    more minor fixes

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const App = () => {
               {/* Apps */}
               <Route path="/kanban" element={<Kanban />} />
               <Route path="/editor" element={<Editor />} />
-              <Route path="/Calendar" element={<Calendar />} />
+              <Route path="/calendar" element={<Calendar />} />
               <Route path="/color-picker" element={<ColorPicker />} />
 
               {/* Charts */}
